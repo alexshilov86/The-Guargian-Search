c01ca6b45d8935d5a5d28bed5b716e509be1ca16
alexshilov86 <avshilov995@gmail.com>
2022-09-13 18:11:46 +0300

    add search with api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import results from "./Results";
-import Search_table from "./Search_box";
+
+import SearchTable from "./Search_box";
+import { ChakraProvider } from '@chakra-ui/react'
+import {React, useState} from "react";
 ///apy-key-guardian = "db361477-2110-4756-ab04-d1ee6dee873c";
 //url = "https://content.guardianapis.com/search?api-key=" + apy-key-guardian
 
 function App() { 
-  
+  const [result, setResult] = useState([]);  
   return (
+
     <div >
-      <Search_table data={results}/>
+      <ChakraProvider>
+      <SearchTable data={result} fillresults={setResult}/>
+      </ChakraProvider>
     </div>
   );
 }
--- src/Search_box.js
+++ src/Search_box.js
@@ -1,19 +1,69 @@
 import React from "react";
-import './style.css'
+import './style.css';
+import results from "./Results";
+import { ArrowRightIcon } from '@chakra-ui/icons'
+import { Input, InputRightElement, Button, InputGroup } from '@chakra-ui/react'
+let apy_key_guardian = "db361477-2110-4756-ab04-d1ee6dee873c";
+function SearchBox(props) {
+    return (
+        <div key={props.data.id} className="searchbox">
+            <div className="summary_style">
+                <span className="sectionname">{props.data.sectionName}</span>
+                <span className="type_style">{props.data.type}</span>
+                <span className="type_style">{props.data.webPublicationDate}</span>
+            </div>
+            <span className="text_style">
+                {props.data.webTitle}
+            </span>
+            <a href={props.data.webUrl} target="blank"><ArrowRightIcon w={7} h={3} color="blue.400" /></a>
+        </div>
+    )
+}
+function getDataFromApi(searchtext, setData) {
+    let url = "https://content.guardianapis.com/search?q=" + searchtext + "&api-key=" + apy_key_guardian
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setData(data.response["results"]);
+        });
 
-function Search_box(props) {
-    return(
+}
+function InputSearch(props) {
+    return (
         <div>
-            <p>{props.data.id}</p>
+            <InputGroup size='md'>
+                <Input id="searchtext" placeholder='text for search' />
+                <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={() => {
+                        let searchtext = document.getElementById('searchtext').value;
+                        // let url = "https://content.guardianapis.com/search?q=" + searchtext + "&api-key=" + apy_key_guardian
+                        // fetch(url)
+                        //     .then((response) => {
+                        //         return response.json();
+                        //     })
+                        //     .then((data) => {
+                        //         props.handleClick(data.response["results"]);
+                        //     });
+                        getDataFromApi(searchtext, props.handleClick);
+                    }}>
+                        Show
+                    </Button>
+                </InputRightElement>
+            </InputGroup>
         </div>
     )
 }
 
-function Search_table(props) {
-    return(
-        <div>
-            {props.data.map(e => <Search_box data={e}/>)}
+function SearchTable(props) {
+    return (
+        <div className="searchtable">
+            <InputSearch handleClick={props.fillresults} />
+            <div className="findtable">
+                {props.data.map(e => <SearchBox data={e} />)}
+            </div>
         </div>
     )
 }
-export default Search_table;
+export default SearchTable;
